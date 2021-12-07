4849ab1133611f52c21f46f7624a281eb58a05d4
pPitohu <59769228+pPitohu@users.noreply.github.com>
2021-12-07 17:04:59 +0300

    import/export fixes

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,31 @@
+import notificationIcon from './notification.svg';
+import filterIcon from './filter.svg';
+import arrowBackIcon from './arrow_back.svg';
+import createIcon from './create.svg';
+import deleteIcon from './delete.svg';
+import favouritesActiveIcon from './favourites_active.svg';
+import favouritesInactiveIcon from './favourites_inactive.svg';
+import arrowDropdownIcon from './arrow_dropdown.svg';
+import homeActiveIcon from './home_active.svg';
+import homeInactiveIcon from './home_inactive.svg';
+import groupsInactiveIcon from './groups_inactive.svg';
+import groupsActiveIcon from './groups_active.svg';
+import profileIcon from './profile.svg';
+import searchIcon from './search.svg';
+
+export {
+    notificationIcon,
+    filterIcon,
+    arrowBackIcon,
+    createIcon,
+    deleteIcon,
+    favouritesActiveIcon,
+    favouritesInactiveIcon,
+    arrowDropdownIcon,
+    homeActiveIcon,
+    homeInactiveIcon,
+    groupsActiveIcon,
+    groupsInactiveIcon,
+    profileIcon,
+    searchIcon
+};
