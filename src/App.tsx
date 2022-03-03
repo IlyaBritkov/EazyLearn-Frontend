da50d7676100426406fe4238224dfa04060a942b
Anthony Filonov <antoniifilonov@gmail.com>
2022-03-04 01:12:06 +0300

    refactored some routes to private

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,32 @@ import React from 'react';
 import {
     Routes,
     Route,
-    useLocation
+    useLocation,
+    Navigate
 } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import { useSelector } from 'react-redux';
 import Loader from './components/Loader';
 import SignForm from './components/SignForm/SignForm';
 import Main from './components/Main/Main';
 
 const App: React.FC = () => {
     const location = useLocation();
+    const user = useSelector((state: {[key: string]: any}) => state.user.user);
+
     return (
         <AnimatePresence exitBeforeEnter>
             <Routes location={location} key={location.pathname}>
                 <Route path="/" element={<Loader />} />
                 <Route path="/login" element={<SignForm />} />
-                <Route path="/home" element={<Main page="home" />} />
-                <Route path="/categories" element={<Main page="categories" />} />
-                <Route path="/favourite" element={<Main page="favourite" />} />
-                <Route path="/profile" element={<Main page="profile" />} />
-                <Route path="/learn" element={<Main page="learn" />} />
-                <Route path="/create-card" element={<Main page="create-card" />} />
-                <Route path="/create-group" element={<Main page="create-group" />} />
-                <Route path="/group/:id" element={<Main page="group-view" />} />
+                <Route path="/home" element={user ? <Main page="home" /> : <Navigate to="/login" />} />
+                <Route path="/categories" element={user ? <Main page="categories" /> : <Navigate to="/login" />} />
+                <Route path="/favourite" element={user ? <Main page="favourite" /> : <Navigate to="/login" />} />
+                <Route path="/profile" element={user ? <Main page="profile" /> : <Navigate to="/login" />} />
+                <Route path="/learn" element={user ? <Main page="learn" /> : <Navigate to="/login" />} />
+                <Route path="/create-card" element={user ? <Main page="create-card" /> : <Navigate to="/login" />} />
+                <Route path="/create-group" element={user ? <Main page="create-group" /> : <Navigate to="/login" />} />
+                <Route path="/group/:id" element={user ? <Main page="group-view" /> : <Navigate to="/login" />} />
                 <Route path="*" element={<div>404 NOT FOUND</div>} />
             </Routes>
         </AnimatePresence>
