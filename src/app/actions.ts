dbb16f1f9c12c6c0815c42dcda2b46aba6c22db7
Ilya Britkov <ilya.britkov@sap.com>
2022-03-17 02:19:25 +0300

    fix 'update user'

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -7,7 +7,7 @@ import qs from 'qs';
 import { BASE_URL } from '../config.js';
 import checkResponse from '../utils/checkResponse';
 import {
-    setGroups, setCards, removeCard, removeGroup
+    setGroups, setCards, removeCard, removeGroup, setUser
 } from './userSlice';
 
 const authHeader = (token: string) => ({ headers: { Authorization: `Bearer ${token}` } });
@@ -234,6 +234,33 @@ export const changeCardStatus: any = createAsyncThunk(
         }
     }
 );
+
+export const updateUserById: any = createAsyncThunk(
+    'user/updateUserById',
+    async (data: any, { rejectWithValue, getState, dispatch }: any) => {
+        try {
+            console.log(data);
+            const { user }: any = getState();
+            const response = await axios.patch(
+                `${BASE_URL}/users/${data.userId}`,
+                {
+                    username: data.username,
+                    email: data.email,
+                    password: data.password,
+                },
+                authHeader(user.token)
+            );
+            console.log('GROUP FAV', response.data);
+            dispatch(setUser([...user.groups, response.data]));
+            console.log(user.groups);
+            return response.data;
+        } catch (error: any) {
+            console.log(error);
+            return rejectWithValue(error.response.data);
+        }
+    }
+);
+
 export const updateGroupById: any = createAsyncThunk(
     'user/updateGroupById',
     async (data: any, { rejectWithValue, getState, dispatch }: any) => {
